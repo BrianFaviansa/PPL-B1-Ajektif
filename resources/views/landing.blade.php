abe0d0e269dfe45328a6bc19533ac6ae3a377e57
BrianFaviansa <brianfpd31@gmail.com>
2024-05-09 10:52:15 +0700

    [fix] fix section kelas belajar

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -12,24 +12,28 @@
     </div>
 
     {{-- section kelas --}}
-    <h2 class="text-4xl font-bold dark:text-white mb-2 md:ml-14">Kelas Belajar</h2>
-    <div class="justify-end flex my-2 md:my-4 md:mr-36 mr-16">
-        <a href="{{ route('landing.kelas.index') }}" class="text-lg md:text-xl text-green-800 font-medium">Selengkapnya</a>
-    </div>
-    <div class="grid grid-cols-1 mx-14 md:grid-cols-3 gap-4 mt-2" id="kelas-belajar">
-        @forelse ($kelasOfflines as $kelasOffline)
-            @include('layouts.landing.card-kelas')
-        @empty
-            Tidak ada kelas offline
-        @endforelse
-    </div>
+    <section id="kelas-belajar">
+        <h2 class="text-4xl font-bold dark:text-white mb-2 md:ml-14">Kelas Belajar</h2>
+        <div class="justify-end flex my-2 md:my-4 md:mr-36 mr-16">
+            <a href="{{ route('landing.kelas.index') }}"
+                class="text-lg md:text-xl text-green-800 font-medium">Selengkapnya</a>
+        </div>
+        <div class="grid grid-cols-1 mx-14 md:grid-cols-3 gap-4 mt-2">
+            @forelse ($kelasOfflines as $kelasOffline)
+                @include('layouts.landing.card-kelas')
+            @empty
+                Tidak ada kelas offline
+            @endforelse
+        </div>
+    </section>
     {{-- section kelas end --}}
 
     {{-- section pelatihan --}}
 
     <h2 class="text-4xl font-bold dark:text-white md:mb-4 mb-2 md:ml-14 mt-10 md:mt-10">Pelatihan Online</h2>
     <div class="justify-end flex my-2 md:my-4 md:mr-36 mr-16">
-        <a href="{{ route('landing.pelatihan.index') }}" class="text-lg md:text-xl text-green-800 font-medium">Selengkapnya</a>
+        <a href="{{ route('landing.pelatihan.index') }}"
+            class="text-lg md:text-xl text-green-800 font-medium">Selengkapnya</a>
     </div>
 
     <div class="grid grid-cols-1 mx-14 md:grid-cols-3 gap-4 mb-6 md:mb-10">
