73827f020f54934bbb1d504286817bbc0deb9a08
BrianFaviansa <brianfpd31@gmail.com>
2024-05-10 17:08:54 +0700

    [feat] done modal create and update

--- resources/views/tambah-akun/create-bpp.blade.php
+++ resources/views/tambah-akun/create-bpp.blade.php
@@ -56,7 +56,6 @@
         </div>
         <a href="{{ route('akun.create') }}"
             class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Kembali</a>
-        <button type="submit"
-            class="max-w-xs focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Simpan</button>
+        @include('layouts.partials.modal-create')
     </form>
 @endsection
