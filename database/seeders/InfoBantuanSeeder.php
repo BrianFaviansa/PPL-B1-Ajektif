a47f5ce571a8e67bc489892f241b155502c3a662
BrianFaviansa <brianfpd31@gmail.com>
2024-05-20 12:37:28 +0700

    [fix] back button crashed with footer

--- database/seeders/InfoBantuanSeeder.php
+++ database/seeders/InfoBantuanSeeder.php
@@ -16,25 +16,32 @@ class InfoBantuanSeeder extends Seeder
         InfoBantuan::create([
             'nama' => 'Bantuan Traktor',
             'ringkasan' => 'Pemerintah menyediakan berbagai macam bantuan untuk sektor pertanian, baik dari pusat maupun daerah. Bantuan ini tersedia dalam bentuk sarana dan prasarana, rehabilitasi/pembangunan gedung/bangunan, dan pembinaan. Tujuannya adalah untuk meningkatkan produktivitas pertanian, kesejahteraan petani, dan memperkuat ketahanan pangan nasional. Informasi lebih lanjut mengenai jenis bantuan, cara mendapatkannya, dan program yang sedang berjalan dapat diperoleh di website Kementerian Pertanian, website Dinas Pertanian dan Perkebunan di daerah Anda, atau dengan menghubungi langsung dinas terkait.',
-            'syarat' => 'Kelompok tani:
-            Memiliki legalitas berupa akta pendirian dan kepengurusan yang sah
-            Memiliki rencana usaha tani (RUTK) yang disetujui oleh Dinas Pertanian dan Perkebunan
-            Memiliki anggota minimal 10 orang
-            Aktif dalam kegiatan kelompok tani
-            Memenuhi persyaratan lain yang ditetapkan oleh Kementerian Pertanian atau Dinas Pertanian dan Perkebunan di daerah Anda
-            Dokumen:
-            KTP Elektronik (KTP-el)
-            Kartu Keluarga (KK)
-            Surat Tanda Usaha Petani (STUP) atau Nomor Usaha Petani (NUP)
-            Bukti kepemilikan atau penguasaan lahan
-            Surat pernyataan aktif bertani
-            Proposal pengajuan bantuan yang berisi:
-            Tujuan dan manfaat bantuan
-            Rencana kegiatan
-            Anggaran biaya
-            Jadwal pelaksanaan
-            Surat pernyataan kesanggupan untuk mengembalikan bantuan jika tidak digunakan sesuai dengan peruntukannya
-            Memenuhi persyaratan lain yang ditetapkan oleh Kementerian Pertanian atau Dinas Pertanian dan Perkebunan di daerah Anda'
+            'syarat' => '<h2>Kelompok tani:</h2>
+            <ul>
+            <li>Memiliki legalitas berupa akta pendirian dan kepengurusan yang sah</li>
+            <li>Memiliki rencana usaha tani (RUTK) yang disetujui oleh Dinas Pertanian dan Perkebunan</li>
+            <li>Memiliki anggota minimal 10 orang</li>
+            <li>Aktif dalam kegiatan kelompok tani</li>
+            <li>Memenuhi persyaratan lain yang ditetapkan oleh Kementerian Pertanian atau Dinas Pertanian dan Perkebunan di daerah Anda</li>
+            </ul>
+            <h2>Dokumen:</h2>
+            <ol>
+            <li>KTP Elektronik (KTP-el)</li>
+            <li>Kartu Keluarga (KK)</li>
+            <li>Surat Tanda Usaha Petani (STUP) atau Nomor Usaha Petani (NUP)</li>
+            <li>Bukti kepemilikan atau penguasaan lahan</li>
+            <li>Surat pernyataan aktif bertani</li>
+
+            <h2>Proposal pengajuan bantuan yang berisi:</h2>
+            </ol>
+            <ol>
+            <li>Tujuan dan manfaat bantuan</li>
+            <li>Rencana kegiatan</li>
+            <li>Anggaran biaya</li>
+            <li>Jadwal pelaksanaan</li>
+            <li>Surat pernyataan kesanggupan untuk mengembalikan bantuan jika tidak digunakan sesuai dengan peruntukannya</li>
+            <li>Memenuhi persyaratan lain yang ditetapkan oleh Kementerian Pertanian atau Dinas Pertanian dan Perkebunan di daerah Anda</li>
+            </ol>'
         ]);
     }
 }
--- resources/views/kelas/landing/show.blade.php
+++ resources/views/kelas/landing/show.blade.php
@@ -91,8 +91,10 @@
         <p class="mb-3 text-gray-500 dark:text-gray-400 text-justify text-base md:text-2xl">{!! $kelasOffline->ringkasan !!}</p>
     </div>
     @role('bpp|dinas')
-    <a href="{{ route('landing.kelas.index') }}"
-        class="py-2.5 px-5 me-2 mb-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Kembali</a>
+        <div class="mb-6">
+            <a href="{{ route('landing.kelas.index') }}"
+                class="py-2.5 px-5 me-2 mb-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Kembali</a>
+        </div>
     @endrole
     @guest
         <div class="container mx-auto mb-4 mt-6">
