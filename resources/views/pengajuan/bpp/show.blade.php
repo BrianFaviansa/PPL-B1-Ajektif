3906e400e7ff181f5d7a268420b6fbd38d7f35f3
BrianFaviansa <brianfpd31@gmail.com>
2024-05-31 13:30:37 +0700

    [fix] document color from blue to green

--- resources/views/pengajuan/bpp/show.blade.php
+++ resources/views/pengajuan/bpp/show.blade.php
@@ -67,7 +67,7 @@
         </div>
         <div class="flex items-center">
             <a href="{{ asset('storage/dokumen_pengajuans/' . $pengajuan->dokumen_pengajuan) }}" target="_blank"
-                class="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                class="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                 <svg class="w-[18px] h-[18px] text-white dark:text-white" aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                     viewBox="0 0 24 24">
